77706bdf9fb19349a4f32e8075f891dec666d2a1
choi122424 <choi122424@krafton.com>
2023-12-10 18:29:36 +0900

    창 띄우기 기능 추가

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,6 +1,31 @@
-    #include <iostream>
+#include <spdlog/spdlog.h>
+#include <GLFW/glfw3.h>
 
-    int main(int argc, const char** argv){
-        std::cout << "Hello, OpenGL world" << std::endl;
-        return 0;
+int main(int argc, const char** argv){
+    SPDLOG_INFO("Start Program");
+
+    SPDLOG_INFO("Initialize glfw");
+    if(!glfwInit()){
+        const char* description = nullptr;
+        glfwGetError(&description);
+        SPDLOG_ERROR("failed to initialize glfw: {}", description);
+        return -1;
     }
+
+    SPDLOG_INFO("Create glfw winodw");
+    auto window = glfwCreateWindow(WINDOW_WIDTH,WINDOW_HEIGHT,WINDOW_NAME, nullptr, nullptr);
+
+    if(!window){
+        SPDLOG_ERROR("fail to create window!");
+        glfwTerminate();
+        return -1;
+    }
+
+    SPDLOG_INFO("Start main loop");
+    while (!glfwWindowShouldClose(window)){
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
